73c0ed7c75ec0a08c612564d8ae4e025d11dd30d
MagaZargaryan <31775113+MagaZargaryan@users.noreply.github.com>
2017-12-01 16:53:04 +0400

    Update Math.java

--- src/utils/Math.java
+++ src/utils/Math.java
@@ -1,11 +1,15 @@
 package utils;
 
 public class Math {
+	
+	        // this will compute the factorial by using recursion
 		public static int factorial(int num){
 		 if(num<=0){
 			 return 1;
 			 }return num * factorial(num-1);
 			    }
+	
+	                // this will compute the factorial by using for loop
 			public static int factorialLoop(int num){
 			    int i;
 			    int fact=num;
@@ -14,6 +18,7 @@ public class Math {
 			     }
 			    return(fact);
 			}
+	                // this code is for checking whether the code works or not
 			public static void main(String[] args){
 			
 			System.out.println(factorial(15));
