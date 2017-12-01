5fd11fbdb8f71ce6134c454e8be444ebf24cae56
MagaZargaryan <31775113+MagaZargaryan@users.noreply.github.com>
2017-12-01 16:48:42 +0400

    Update Puppy.java

--- src/animals/animals/Puppy.java
+++ src/animals/animals/Puppy.java
@@ -3,17 +3,17 @@ package animals.animals;
 public class Puppy {
     private String name;
     public Puppy(String name){
-    this.name=name;
+    this.name=name;  // this is for discriminating between two variables called name
     } 
 public String getName(){
-return ( "Name of Puppy is " +this.name);
+return ( "Name of Puppy is " +this.name); // this will print   - "Name of Puppy is "and the given name" "
 }
 
 
-
+// this main method is for checking whether the code works or not
 public static void main(String[] args){
  Puppy a = new Puppy("Joe");
-System.out.println(a.getName());
+System.out.println(a.getName()); // this will print   - "Name of Puppy is Joe "
 
 }
 }
